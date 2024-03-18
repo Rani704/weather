35d535dffa5190d4f2f8320b104d97ffee4a6842
Rani <rani7042003@gmail.com>
2024-03-18 15:26:52 +0530

    first commit

---
+++ weatherapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Resturant from './components/Basics/Resturant';
+//  import UseState from "./components/Hooks/useState";
+// import UseEffect from './components/Hooks/useEffect';
+// import UseReducer from './components/Hooks/useReducer';
+// import Todo from './components/todoreact/todo';
+import Temp from './components/weather/temp.js'
+const App = () => {
+  return (
+    <>
+    <Temp/>
+    </>
+  );
+};
+
+
+export default App;
